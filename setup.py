cbf58cca3a04070ac70de8180b43db987517c488
Ultr4_dev <rustedofficial1337@gmail.com>
2024-12-24 09:12:30 +0100

    bump version to 0.1.4 in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Sea",
-    version="0.1.3",
+    version="0.1.4",
     author="Ultr4",
     author_email="ultra@ultra.io",
     packages=find_packages(),
